25fa2660f9e5f06782fd5c9895ff4a97893c3bcd
Hugo Oliveira <hugooliveira.dev@proton.me>
2025-10-31 22:27:10 -0300

    feat: add repl

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"regexp"
 	"strings"
 )
@@ -9,11 +12,26 @@ func cleanInput(text string) []string {
 	spaceRegex := regexp.MustCompile(`\s+`)
 	clearString := strings.ToLower(strings.TrimSpace(spaceRegex.ReplaceAllString(text, " ")))
 
-	commands := strings.Split(clearString, " ")
+	terms := strings.Split(clearString, " ")
 
-	return commands
+	return terms
 }
 
 func main() {
 
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		if err := scanner.Err(); err != nil {
+			fmt.Printf("Error reading input: %v\n", err)
+			return
+		}
+
+		terms := cleanInput(input)
+
+		fmt.Printf("Your command was: %s\n", terms[0])
+	}
 }
